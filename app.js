62389b78f63dffca4bed917d17da69605a4a93e8
Kuro <mujina7569@gmail.com>
2023-03-18 02:30:17 +0700

    Update app.js
    remove comment

--- app.js
+++ app.js
@@ -1,49 +1,39 @@
-// declare global variable
-let video = null // video element
-let detector = null // detector object
-let detections = [] // store detection result
+let video = null
+let detector = null
+let detections = []
 let videoVisibility = true
 let detecting = false
 let canvas
 
-// global HTML element
 const toggleVideoEl = document.getElementById('toggleVideoEl')
 const toggleDetectingEl = document.getElementById('toggleDetectingEl')
 const labelEl = document.getElementById('label')
 
-// set cursor to wait until video elment is loaded
 document.body.style.cursor = 'wait'
 
-// The preload() function if existed is called before the setup() function
 function preload() {
   const detectionOptions = {
     withLandmarks: true,
     withDescriptors: false,
   }
-  // Initialize the magicFeature
   detector = ml5.faceApi(detectionOptions, modelLoaded)
 }
-// When the model is loaded
+
 function modelLoaded() {
   console.log('Model Loaded!')
 
-  // Make some sparkles
   faceapi.detect(myImage, (err, results) => {
     console.log(results)
   })
 }
 
-// The setup() function is called once when the program starts.
 function setup() {
-  // create canvas element with 640 width and 480 height in pixel
   canvas = createCanvas(640, 480)
-  // Creates a new HTML5 <video> element that contains the audio/video feed from a webcam.
-  // The element is separate from the canvas and is displayed by default.
   video = createCapture(VIDEO)
   video.size(640, 480)
   console.log('video element is created')
   video.elt.addEventListener('loadeddata', function () {
-    // set cursor back to default
+
     if (video.elt.readyState >= 2) {
       document.body.style.cursor = 'default'
       console.log(
@@ -51,81 +41,54 @@ function setup() {
       )
     }
   })
+  video.hide();
 }
 
-// the draw() function continuously executes until the noLoop() function is called
 function draw() {
   if (!video || !detecting) return
-  // draw video frame to canvas and place it at the top-left corner
+
   image(video, 0, 0)
-  // draw all detected objects to the canvas
+
   for (let i = 0; i < detections.length; i++) {
     drawResult(detections[i])
   }
 }
 
-/*
-Exaple of an detect object
-{
-    "label": "person",
-    "confidence": 0.8013999462127686,
-    "x": 7.126655578613281,
-    "y": 148.3782720565796,
-    "width": 617.7880859375,
-    "height": 331.60210132598877,
-}
-*/
 function drawResult(object) {
   drawBoundingBox(object)
   drawLabel(object)
 }
 
-// draw bounding box around the detected object
 function drawBoundingBox(object) {
-  // Sets the color used to draw lines.
   stroke('green')
-  // width of the stroke
   strokeWeight(4)
-  // Disables filling geometry
   noFill()
-  // draw an rectangle
-  // x and y are the coordinates of upper-left corner, followed by width and height
   rect(object.x, object.y, object.width, object.height)
 }
 
-// draw label of the detected object (inside the box)
 function drawLabel(object) {
-  // Disables drawing the stroke
   noStroke()
-  // sets the color used to fill shapes
   fill('white')
-  // set font size
   textSize(24)
-  // draw string to canvas
   text(object.label, object.x + 10, object.y + 24)
 }
 
-// callback function. it is called when object is detected
 function onDetected(error, results) {
   if (error) {
     console.error(error)
   }
   detections = results
 
-  // Face detected, hide canvas
   if (detections.length > 0) {
     hideVieoElAfterDetected()
   }
 
-  // keep detecting object
   if (detecting) {
     detect()
   }
 }
 
 function detect() {
-  // instruct "detector" object to start detect object from video element
-  // and "onDetected" function is called when object is detected
   detector.detect(video, onDetected)
 }
 
